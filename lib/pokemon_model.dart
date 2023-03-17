35b8790773852bd928c43ac4e5493ee9ab50a2ab
caleb <env_foodtable@naver.com>
2023-03-17 16:09:17 +0900

    assert 쉽게 변경

--- lib/pokemon_model.dart
+++ lib/pokemon_model.dart
@@ -9,62 +9,100 @@ class PokemonModel {
     required this.imageAddress,
     required this.indexNum,
     required this.skills,
-  })  : assert(indexNum > 0),
-        assert(skills.length <= 4),
-        assert(skills.isNotEmpty);
+  }) {
+    assert(indexNum > 0);
+    assert(skills.isNotEmpty);
+    print('PokemonModel 객체 생성 완료 $name');
+  }
 
   static List<PokemonModel> samples = [
-    PokemonModel(name: '피카츄', imageAddress: 'assets/images/pikachu.png', indexNum: 25, skills: [
-      SkillModel(name: '전기쇼크', damage: 40, pp: 30, learningLevel: 1),
-      SkillModel(name: '전광석화', damage: 40, pp: 30, learningLevel: 11),
-      SkillModel(name: '10만볼트', damage: 95, pp: 15, learningLevel: 26),
-    ]),
-    PokemonModel(name: '이상해씨', imageAddress: 'assets/images/bulbasaur.png', indexNum: 1, skills: [
-      SkillModel(name: '몸통박치기', damage: 35, pp: 35, learningLevel: 1),
-      SkillModel(name: '덩굴채찍', damage: 35, pp: 10, learningLevel: 10),
-      SkillModel(name: '수면가루', damage: 0, pp: 15, learningLevel: 15),
-      SkillModel(name: '솔라빔', damage: 120, pp: 10, learningLevel: 46),
-    ]),
-    PokemonModel(name: '파이리', imageAddress: 'assets/images/charmander.png', indexNum: 4, skills: [
-      SkillModel(name: '할퀴기', damage: 40, pp: 35, learningLevel: 1),
-      SkillModel(name: '불꽃세례', damage: 40, pp: 25, learningLevel: 7),
-      SkillModel(name: '화염방사', damage: 95, pp: 15, learningLevel: 31),
-      SkillModel(name: '회오리불꽃', damage: 15, pp: 15, learningLevel: 49),
-    ]),
-    PokemonModel(name: '꼬부기', imageAddress: 'assets/images/squirtle.png', indexNum: 7, skills: [
-      SkillModel(name: '몸통박치기', damage: 35, pp: 35, learningLevel: 1),
-      SkillModel(name: '거품', damage: 20, pp: 30, learningLevel: 7),
-      SkillModel(name: '물대포', damage: 40, pp: 25, learningLevel: 13),
-      SkillModel(name: '로케트박치기', damage: 100, pp: 15, learningLevel: 40),
-    ]),
-    PokemonModel(name: '갸라도스', imageAddress: 'assets/images/gyarados.png', indexNum: 130, skills: [
-      SkillModel(name: '난동부리기', damage: 90, pp: 20, learningLevel: 1),
-      SkillModel(name: '물기', damage: 60, pp: 25, learningLevel: 20),
-      SkillModel(name: '파괴광선', damage: 150, pp: 5, learningLevel: 50),
-      SkillModel(name: '하이드로펌프', damage: 120, pp: 5, learningLevel: 40),
-    ]),
-    PokemonModel(name: '망나뇽', imageAddress: 'assets/images/dragonite.png', indexNum: 149, skills: [
-      SkillModel(name: '회오리', damage: 40, pp: 20, learningLevel: 15),
-      SkillModel(name: '용의분노', damage: 40, pp: 10, learningLevel: 22),
-      SkillModel(name: '역린', damage: 90, pp: 15, learningLevel: 61),
-      SkillModel(name: '파괴광선', damage: 150, pp: 5, learningLevel: 75),
-    ]),
-    PokemonModel(name: '잠만보', imageAddress: 'assets/images/snorlax.png', indexNum: 143, skills: [
-      SkillModel(name: '박치기', damage: 70, pp: 15, learningLevel: 29),
-      SkillModel(name: '잠자기', damage: 0, pp: 10, learningLevel: 36),
-      SkillModel(name: '코골기', damage: 40, pp: 15, learningLevel: 36),
-      SkillModel(name: '구르기', damage: 30, pp: 20, learningLevel: 50),
-    ]),
-    PokemonModel(name: '팬텀', imageAddress: 'assets/images/haunter.png', indexNum: 94, skills: [
-      SkillModel(name: '핥기', damage: 20, pp: 30, learningLevel: 1),
-      SkillModel(name: '최면술', damage: 0, pp: 20, learningLevel: 1),
-      SkillModel(name: '꿈먹기', damage: 100, pp: 15, learningLevel: 39),
-    ]),
-    PokemonModel(name: '전룡', imageAddress: 'assets/images/ampharos.png', indexNum: 181, skills: [
-      SkillModel(name: '번개펀치', damage: 75, pp: 15, learningLevel: 30),
-      SkillModel(name: '빛의장막', damage: 0, pp: 30, learningLevel: 42),
-      SkillModel(name: '번개', damage: 120, pp: 10, learningLevel: 57),
-    ]),
+    PokemonModel(
+        name: '피카츄',
+        imageAddress: 'assets/images/pikachu.png',
+        indexNum: 25,
+        skills: [
+          SkillModel(name: '전기쇼크', damage: 40, pp: 30, learningLevel: 1),
+          SkillModel(name: '전광석화', damage: 40, pp: 30, learningLevel: 11),
+          SkillModel(name: '10만볼트', damage: 95, pp: 15, learningLevel: 26),
+        ]),
+    PokemonModel(
+        name: '이상해씨',
+        imageAddress: 'assets/images/bulbasaur.png',
+        indexNum: 1,
+        skills: [
+          SkillModel(name: '몸통박치기', damage: 35, pp: 35, learningLevel: 1),
+          SkillModel(name: '덩굴채찍', damage: 35, pp: 10, learningLevel: 10),
+          SkillModel(name: '수면가루', damage: 0, pp: 15, learningLevel: 15),
+          SkillModel(name: '솔라빔', damage: 120, pp: 10, learningLevel: 46),
+        ]),
+    PokemonModel(
+        name: '파이리',
+        imageAddress: 'assets/images/charmander.png',
+        indexNum: 4,
+        skills: [
+          SkillModel(name: '할퀴기', damage: 40, pp: 35, learningLevel: 1),
+          SkillModel(name: '불꽃세례', damage: 40, pp: 25, learningLevel: 7),
+          SkillModel(name: '화염방사', damage: 95, pp: 15, learningLevel: 31),
+          SkillModel(name: '회오리불꽃', damage: 15, pp: 15, learningLevel: 49),
+        ]),
+    PokemonModel(
+        name: '꼬부기',
+        imageAddress: 'assets/images/squirtle.png',
+        indexNum: 7,
+        skills: [
+          SkillModel(name: '몸통박치기', damage: 35, pp: 35, learningLevel: 1),
+          SkillModel(name: '거품', damage: 20, pp: 30, learningLevel: 7),
+          SkillModel(name: '물대포', damage: 40, pp: 25, learningLevel: 13),
+          SkillModel(name: '로케트박치기', damage: 100, pp: 15, learningLevel: 40),
+        ]),
+    PokemonModel(
+        name: '갸라도스',
+        imageAddress: 'assets/images/gyarados.png',
+        indexNum: 130,
+        skills: [
+          SkillModel(name: '난동부리기', damage: 90, pp: 20, learningLevel: 1),
+          SkillModel(name: '물기', damage: 60, pp: 25, learningLevel: 20),
+          SkillModel(name: '파괴광선', damage: 150, pp: 5, learningLevel: 50),
+          SkillModel(name: '하이드로펌프', damage: 120, pp: 5, learningLevel: 40),
+        ]),
+    PokemonModel(
+        name: '망나뇽',
+        imageAddress: 'assets/images/dragonite.png',
+        indexNum: 149,
+        skills: [
+          SkillModel(name: '회오리', damage: 40, pp: 20, learningLevel: 15),
+          SkillModel(name: '용의분노', damage: 40, pp: 10, learningLevel: 22),
+          SkillModel(name: '역린', damage: 90, pp: 15, learningLevel: 61),
+          SkillModel(name: '파괴광선', damage: 150, pp: 5, learningLevel: 75),
+        ]),
+    PokemonModel(
+        name: '잠만보',
+        imageAddress: 'assets/images/snorlax.png',
+        indexNum: 143,
+        skills: [
+          SkillModel(name: '박치기', damage: 70, pp: 15, learningLevel: 29),
+          SkillModel(name: '잠자기', damage: 0, pp: 10, learningLevel: 36),
+          SkillModel(name: '코골기', damage: 40, pp: 15, learningLevel: 36),
+          SkillModel(name: '구르기', damage: 30, pp: 20, learningLevel: 50),
+        ]),
+    PokemonModel(
+        name: '팬텀',
+        imageAddress: 'assets/images/haunter.png',
+        indexNum: 94,
+        skills: [
+          SkillModel(name: '핥기', damage: 20, pp: 30, learningLevel: 1),
+          SkillModel(name: '최면술', damage: 0, pp: 20, learningLevel: 1),
+          SkillModel(name: '꿈먹기', damage: 100, pp: 15, learningLevel: 39),
+        ]),
+    PokemonModel(
+        name: '전룡',
+        imageAddress: 'assets/images/ampharos.png',
+        indexNum: 181,
+        skills: [
+          SkillModel(name: '번개펀치', damage: 75, pp: 15, learningLevel: 30),
+          SkillModel(name: '빛의장막', damage: 0, pp: 30, learningLevel: 42),
+          SkillModel(name: '번개', damage: 120, pp: 10, learningLevel: 57),
+        ]),
   ];
 }
 
@@ -73,5 +111,9 @@ class SkillModel {
   int learningLevel; //배우는 레벨
   double damage; //데미지
   int pp; //스킬 사용 가능 횟수
-  SkillModel({required this.learningLevel, required this.name, required this.damage, required this.pp});
+  SkillModel(
+      {required this.learningLevel,
+      required this.name,
+      required this.damage,
+      required this.pp});
 }
