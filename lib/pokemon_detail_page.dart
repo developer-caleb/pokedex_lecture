e20e9fa2ee770db74b645e6583ddd72597c91ea7
yongik <env_foodtable@naver.com>
2023-03-30 19:42:02 +0900

    complete

--- lib/pokemon_detail_page.dart
+++ lib/pokemon_detail_page.dart
@@ -80,31 +80,34 @@ class _RecipeDetailPageState extends State<PokemonDetailPage> {
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-            child: Table(
-              border: TableBorder.all(),
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              children: <TableRow>[
-                const TableRow(children: [
-                  Center(child: Text('LV')),
-                  Center(child: Text('기술')),
-                  Center(child: Text('위력')),
-                  Center(child: Text('PP')),
-                ]),
-                ...List.generate(widget.pokemon.skills.where((element) => lv >= element.learningLevel).length, (index) {
-                  List<SkillModel> skillSet =
-                      widget.pokemon.skills.where((element) => lv >= element.learningLevel).toList();
-                  return TableRow(children: [
-                    Center(child: Text(skillSet[index].learningLevel.toString())),
-                    Center(child: Text(skillSet[index].name.toString())),
-                    Center(child: Text(skillSet[index].damage.toString())),
-                    Center(child: Text(skillSet[index].pp.toString())),
-                  ]);
-                })
-              ],
-            ),
+            child: buildTable(),
           )
         ],
       )),
+    );
+  }
+
+  Table buildTable() {
+    List<SkillModel> skillSet = widget.pokemon.skills.where((element) => lv >= element.learningLevel).toList();
+    return Table(
+      border: TableBorder.all(),
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: <TableRow>[
+        const TableRow(children: [
+          Center(child: Text('LV')),
+          Center(child: Text('기술')),
+          Center(child: Text('위력')),
+          Center(child: Text('PP')),
+        ]),
+        ...List.generate(skillSet.length, (index) {
+          return TableRow(children: [
+            Center(child: Text(skillSet[index].learningLevel.toString())),
+            Center(child: Text(skillSet[index].name.toString())),
+            Center(child: Text(skillSet[index].damage.toString())),
+            Center(child: Text(skillSet[index].pp.toString())),
+          ]);
+        })
+      ],
     );
   }
 }
